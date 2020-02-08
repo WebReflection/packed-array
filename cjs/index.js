9039ce7e3f1809243c3cbe834a2ca6453e94c048
WebReflection <andrea.giammarchi@gmail.com>
2020-02-08 11:19:37 +0100

    added JSDocs to the function

--- cjs/index.js
+++ cjs/index.js
@@ -1,11 +1,19 @@
 'use strict';
-module.exports = (sizer, fn, self) => {
+/**
+ * An utility to created packed arrays 
+ *
+ * @param {(number | Array)} sizer - Either the length of the new Array or an Array
+ * @param {(Function | any)} filler - Either a function to invoke per each index, or a value to fill the Array
+ * @param {any=undefined} self - The context to use via the `filler`, if it's a Function
+ * @return {Array<filler>} The Array of length `sizer` filled via the `filler`
+ */
+module.exports = (sizer, filler, self) => {
   const out = [];
   for (let
-    isFunction = typeof fn === 'function',
+    isFunction = typeof filler === 'function',
     length = typeof sizer === 'number' ? sizer : sizer.length,
     i = 0; i < length; i++
   )
-    out.push(isFunction ? fn.call(self, sizer[i], i) : fn);
+    out.push(isFunction ? filler.call(self, sizer[i], i) : filler);
   return out;
 };
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var packedArray = (function (exports) {
   'use strict';
 
-  var index = (function (sizer, fn, self) {
+  /**
+   * An utility to created packed arrays 
+   *
+   * @param {(number | Array)} sizer - Either the length of the new Array or an Array
+   * @param {(Function | any)} filler - Either a function to invoke per each index, or a value to fill the Array
+   * @param {any=undefined} self - The context to use via the `filler`, if it's a Function
+   * @return {Array<filler>} The Array of length `sizer` filled via the `filler`
+   */
+  var index = (function (sizer, filler, self) {
     var out = [];
 
-    for (var isFunction = typeof fn === 'function', length = typeof sizer === 'number' ? sizer : sizer.length, i = 0; i < length; i++) {
-      out.push(isFunction ? fn.call(self, sizer[i], i) : fn);
+    for (var isFunction = typeof filler === 'function', length = typeof sizer === 'number' ? sizer : sizer.length, i = 0; i < length; i++) {
+      out.push(isFunction ? filler.call(self, sizer[i], i) : filler);
     }
 
     return out;
